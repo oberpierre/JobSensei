ed72f3898018df27e44daec22048abce3512b8f8
Pierre Obermaier <pierre.obermaier@gmail.com>
2023-08-17 13:11:10 +0200

    F Integrating scrapy

--- scraper/src/main.py
+++ scraper/src/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    print("Begin scraping... (to be implemented)")
+from scrapy.crawler import CrawlerProcess
+from scraper.src.spiders.quotes_spider import QuotesSpider
+
+if __name__ == '__main__':
+    process = CrawlerProcess(settings={
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'output.json'
+    })
+
+    process.crawl(QuotesSpider)
+    process.start()
