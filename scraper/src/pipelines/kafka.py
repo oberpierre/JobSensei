09a708953d37f4e065a3a466637d18e423c490c8
Pierre Obermaier <pierre.obermaier@gmail.com>
2023-10-08 15:43:43 +0000

    F Using confluent-kafka over kafka-python

--- scraper/src/pipelines/kafka.py
+++ scraper/src/pipelines/kafka.py
@@ -1,6 +1,6 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 import json
 import uuid
 from datetime import datetime
@@ -10,10 +10,11 @@ class KafkaPipeline:
         self.start_topic = start_topic
         self.end_topic = end_topic
         self.sourcing_topic = sourcing_topic
-        self.producer = KafkaProducer(
-            bootstrap_servers=kafka_bootstrap_servers,
-            value_serializer=lambda x: json.dumps(x).encode("utf-8")
-        )
+        conf = {
+            'bootstrap.servers': kafka_bootstrap_servers,
+            'client.id': 'jobsense-scraper',
+        }
+        self.producer = Producer(**conf)
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -26,24 +27,23 @@ class KafkaPipeline:
         )
 
     def open_spider(self, spider): 
-        spider.run_id = str(uuid.uuid4())
-        message = {"status": "start", "timestamp": datetime.now().isoformat(), "runId": spider.run_id}
+        self.run_id = str(uuid.uuid4())
+        message = {"status": "start", "timestamp": datetime.now().isoformat(), "runId": self.run_id}
         self.send_message(self.start_topic, message)
 
     def close_spider(self, spider):
-        message = {"status": "end", "timestamp": datetime.now().isoformat(), "runId": spider.run_id}
+        message = {"status": "end", "timestamp": datetime.now().isoformat(), "runId": self.run_id}
         self.send_message(self.end_topic, message)
         self.producer.flush()
-        self.producer.close()
 
     def process_item(self, item, spider):
         data = ItemAdapter(item)
-        data["runId"] = spider.run_id
+        data["runId"] = self.run_id
         self.send_message(self.sourcing_topic, data.asdict())
         return item
 
     def send_message(self, topic, message):
         try:
-            self.producer.send(topic, message)
+            self.producer.produce(topic, key=self.run_id.encode('utf-8'), value=json.dumps(message).encode('utf-8'))
         except Exception as e:
             print("Error:", e)
