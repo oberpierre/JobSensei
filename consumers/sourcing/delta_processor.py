0b1b56177bb47446c26b5a27d705014408569a48
Pierre Obermaier <pierre.obermaier@gmail.com>
2023-10-10 15:14:10 +0000

    F Adding initial delta processor

---
+++ consumers/sourcing/delta_processor.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class DeltaProcessor:
+    def __init__(self, init_data):
+        self.conn = sqlite3.connect(':memory:')
+        cursor = self.conn.cursor()
+
+        self._init_table(cursor)
+
+
+        for record in init_data:
+            self._insert_data(record, cursor)
+
+        self.conn.commit()
+        cursor.close()
+
+    def __del__(self):
+        self.conn.close()
+        
+
+    def _init_table(self, cursor):
+        cursor.execute('''
+CREATE TABLE IF NOT EXISTS listings (
+    url TEXT PRIMARY KEY,
+    reference TEXT
+)
+''')
+
+    def _insert_data(self, record, cursor):
+        cursor.execute('''
+INSERT INTO listings (url)
+VALUES (:url)''', record)
