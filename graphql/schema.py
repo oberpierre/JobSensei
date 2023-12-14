e431ed581cb6be031444c665ecabfb2262d43a94
Pierre Obermaier <pierre.obermaier@gmail.com>
2023-12-14 16:13:31 +0000

    F Having initial jobs connecting to mongodb

--- graphql/schema.py
+++ graphql/schema.py
@@ -1,9 +1,26 @@
-from graphene import ObjectType, String, Schema
+from graphene import Field, List, ObjectType, String, Schema
+from pymongo import MongoClient
+from decouple import config
+from urllib.parse import quote_plus
+
+user = config('MONGO_USER', default='')
+password = config('MONGO_PASSWORD', default='')
+server = config('MONGO_SERVER', default='localhost:27017')
+db = config('MONGO_DB', default='jobsensei')
+
+conn_str = 'mongodb://%s:%s@%s/%s' % (quote_plus(user), quote_plus(password), quote_plus(server), quote_plus(db)) if user and password else 'mongodb://%s/%s' % (quote_plus(server), quote_plus(db))
+
+client = MongoClient(conn_str)
+
+class Job(ObjectType):
+    uuid = String()
+    title = String()
 
 class Query(ObjectType):
-    hello = String(name=String(default_value='World'))
+    jobs = List(Job)
 
-    def resolve_hello(root, info, name):
-        return f'Hello {name}!'
+    def resolve_jobs(root, info):
+        jobs = client.jobsensei.listings_categorized.find()
+        return [Job(uuid=job['uuid'], title=job['jobTitle']) for job in jobs]
 
 schema = Schema(query=Query)
