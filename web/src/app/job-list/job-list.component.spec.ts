808ab0fecc5eecddc4b9b8c8254a5ce824c094e0
Pierre Obermaier <pierre.obermaier@gmail.com>
2023-12-17 14:00:22 +0000

    R Writing tests for job list

--- web/src/app/job-list/job-list.component.spec.ts
+++ web/src/app/job-list/job-list.component.spec.ts
@@ -1,7 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { JobListComponent } from './job-list.component';
 import { NbStatusService } from '@nebular/theme';
+import { JobCardComponent } from '../job-card/job-card.component';
 
 class MockNbStatusService {
   isCustomStatus = () => false;
@@ -11,18 +13,32 @@ describe('JobListComponent', () => {
   let component: JobListComponent;
   let fixture: ComponentFixture<JobListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      providers: [JobListComponent, {provide: NbStatusService, useClass: MockNbStatusService}]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [JobListComponent],
+      providers: [{provide: NbStatusService, useClass: MockNbStatusService}]
     })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(JobListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(JobListComponent);
+      component = fixture.componentInstance;
+      component.jobs = [
+        {title: 'foo', summary: 'bar'},
+        {title: 'fizzbuzz'}
+      ];
+      fixture.detectChanges();
+    });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should', () => {
+    const cards = fixture.debugElement.queryAll(By.directive(JobCardComponent));
+    const jobs = cards.map((card) => card.componentInstance.job);
+    expect(jobs.length).toBe(2);
+    expect(jobs[0]).toEqual({title: 'foo', summary: 'bar'});
+    expect(jobs[1]).toEqual({title: 'fizzbuzz'});
+  })
 });
