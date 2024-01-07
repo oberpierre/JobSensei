7f65abf4f1061efa40a16c1d5a86f3eb8467f688
Pierre Obermaier <pierre.obermaier@gmail.com>
2024-01-07 08:54:38 +0000

    F Querying job details using graphql

--- web/src/app/job/job.component.spec.ts
+++ web/src/app/job/job.component.spec.ts
@@ -2,17 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { of } from 'rxjs';
+import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
+import { NbStatusService } from '@nebular/theme';
 
-import { JobComponent } from './job.component';
+import { JobComponent, GET_JOB } from './job.component';
 
+class MockNbStatusService {
+  isCustomStatus = () => false;
+}
 describe('JobComponent', () => {
   let component: JobComponent;
   let fixture: ComponentFixture<JobComponent>;
+  let controller: ApolloTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, JobComponent],
+      imports: [RouterTestingModule, ApolloTestingModule, JobComponent],
       providers: [
+        {provide: NbStatusService, useClass: MockNbStatusService},
         {
           provide: ActivatedRoute,
           useValue: {
@@ -26,12 +33,37 @@ describe('JobComponent', () => {
     fixture = TestBed.createComponent(JobComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    controller = TestBed.inject(ApolloTestingController);
   });
 
   it('should create Job component', () => {
+    const op = controller.expectOne(GET_JOB);
+    op.flush({
+      data: {
+        job: null
+      },
+    });
+
     expect(component).toBeTruthy();
   });
-  it('should display job id from url param', () => {
-    expect(fixture.nativeElement.textContent).toBe('job works! id: a0c1fe12-cec2-442a-9329-2cb47f761303')
+  it('should fetch job and display details', async () => {
+    const op = controller.expectOne(GET_JOB);
+    op.flush({
+      data: {
+        job: {
+          title: 'foobar'
+        },
+      },
+    });
+
+    await fixture.whenStable();
+
+    expect(op.operation.variables['uuid']).toBe('a0c1fe12-cec2-442a-9329-2cb47f761303');
+    expect(fixture.nativeElement.textContent).toBe("Loading...");
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.textContent).toBe('foobar')
   })
 });
--- web/src/app/job/job.component.ts
+++ web/src/app/job/job.component.ts
@@ -1,27 +1,53 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { NbCardModule } from '@nebular/theme';
+import { Apollo, gql } from 'apollo-angular';
+import { Job } from '../job';
+
+export const GET_JOB = gql`
+  query GetJob($uuid: String!) {
+    job(uuid: $uuid) {
+      title
+    }
+  }
+`;
 
 @Component({
   selector: 'app-job',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NbCardModule, CommonModule],
   templateUrl: './job.component.html',
   styleUrl: './job.component.css'
 })
 export class JobComponent {
-  jobId$: Observable<string> | undefined;
+  job: Job | undefined;
+  loading: boolean = true;
+  error: any;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private apollo: Apollo) {}
 
   ngOnInit() {
-    this.jobId$ = this.route.paramMap.pipe(
+    this.route.paramMap.pipe(
       switchMap((params: ParamMap) => {
         const id = params.get('id');
         return of(id ? id : "");
       })
-    );
+    ).subscribe((uuid) => {
+      this.apollo
+        .watchQuery<{job: Job}>({
+          query: GET_JOB,
+          variables: {
+            uuid: uuid,
+          }
+        })
+        .valueChanges.subscribe(({data, error, loading}) => {
+          this.job = data?.job ?? undefined;
+          this.loading = loading;
+          this.error = error;
+        });
+    });
   }
 }
