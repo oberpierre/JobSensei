08fc246a9643831ed2a5963936f9fde9c151e5d8
Pierre Obermaier <pierre.obermaier@gmail.com>
2024-02-17 11:23:22 +0000

    R Writing tests for theme change

--- web/src/app/theme-toggle/theme-toggle.component.spec.ts
+++ web/src/app/theme-toggle/theme-toggle.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ThemeToggleComponent } from './theme-toggle.component';
-import { NbLayoutDirectionService, NbStatusService, NbThemeService } from '@nebular/theme';
+import { NbLayoutDirectionService, NbStatusService, NbThemeService, NbToggleComponent } from '@nebular/theme';
 import { of } from 'rxjs';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -13,21 +13,20 @@ class MockNbLayoutDirectionService {
 class MockNbStatusService {
   isCustomStatus = () => false;
 }
-class MockNbThemeService {
-  changeTheme = () => {};
-}
 
 describe('ThemeToggleComponent', () => {
   let component: ThemeToggleComponent;
   let fixture: ComponentFixture<ThemeToggleComponent>;
+  let nbThemeServiceSpy: jasmine.SpyObj<NbThemeService>;
 
   beforeEach(async () => {
+    nbThemeServiceSpy = jasmine.createSpyObj(NbThemeService, ['changeTheme']);
     await TestBed.configureTestingModule({
       imports: [NoopAnimationsModule, ThemeToggleComponent],
       providers: [
         {provide: NbLayoutDirectionService, useClass: MockNbLayoutDirectionService},
         {provide: NbStatusService, useClass: MockNbStatusService},
-        {provide: NbThemeService, useClass: MockNbThemeService},
+        {provide: NbThemeService, useValue: nbThemeServiceSpy},
       ]
     })
     .compileComponents();
@@ -40,4 +39,20 @@ describe('ThemeToggleComponent', () => {
   it('should create theme toggle', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should change the theme upon toggling', async () => {
+    const toggle = fixture.nativeElement.querySelector('input');
+    
+    expect(toggle?.checked).toBe(false);
+
+    toggle?.click();
+    expect(toggle?.checked).toBe(true);
+    expect(nbThemeServiceSpy.changeTheme).toHaveBeenCalledTimes(1);
+    expect(nbThemeServiceSpy.changeTheme.calls.argsFor(0)).toEqual(['default']);
+
+    toggle?.click();
+    expect(toggle?.checked).toBe(false);
+    expect(nbThemeServiceSpy.changeTheme).toHaveBeenCalledTimes(2);
+    expect(nbThemeServiceSpy.changeTheme.calls.argsFor(1)).toEqual(['dark']);
+  })
 });
